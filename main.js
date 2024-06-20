3afca275af5c75959cbbe9592cdbb32b442d5f9e
yuriZz3ddy <132253210+yuriZz3ddy@users.noreply.github.com>
2024-06-20 17:07:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 let addr;
 function main() {
+	var onCreate = location.search;
+	var textCode = new URLSearchParams(onCreate)
+	var onVid = textCode.get("image_id");
+	if (!onVid) {
+		addr = document.querySelector("raw")
+		addr.textContent = "Error NONE"
+		return;
+	} else {
+		
   const mod = {
     page: 1,
     page_size: 60,
-    id: "",
+    id: onVid,
     time_to: 0
   }
 
@@ -11,25 +20,28 @@ async = fetch("https://www.seaart.ai/api/v1/artwork/recommend", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
-  }
+  },
+  body: JSON.stringify(mod)
+  })
   .then(res => {
     return res.json()
   })
   .then(data => {
     var a = data.data.items
+    var jsonp = JSON.stringify(a)
     for (var i = 0; i < a.length; i ++) {
-      var imageUrl = a[i].cover
       addr = document.querySelector("raw")
-      addr.textContent = data;
+      addr.textContent = jsonp;
     }
 
   })
   .catch(er => {
     setTimeout(() => { 
-    addr document.querySelector("raw")
+    addr = document.querySelector("raw")
     addr.textContent = er;
       main()
     }, 2000)
   })
+  }
 }
  main()
